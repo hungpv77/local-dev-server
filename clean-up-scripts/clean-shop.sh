6d5186761f673fbeffe22fb893ba3f97c8997a35
hungpv77 <hung@fabfitfun.com>
2017-04-28 15:48:50 +0700

    Clean shop

--- clean-up-scripts/clean-shop.sh
+++ clean-up-scripts/clean-shop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#The dev server public ip
+dev_server_ip="76.80.27.12"
+domain="fffdev.com"
+hosted_zone_id="Z1986QIYBBYSUJ"
+
+www_dir="/var/www/html/"
 jenkins_workspace="/var/lib/jenkins/workspace/test/"
 apache_vhost_dir="/etc/apache2/sites-available/"
 reponame="shop"
@@ -40,9 +47,15 @@ main(){
     # replace - by _
     project_name=$(echo $project_name | sed 's@-@_@g')
     echo "INFO Project-Name: $project_name"
-
-    
-
+	
+	shop_db_name="${project_name}_${shop_db}"
+    core_db_name="${project_name}_${core_db}"
+	domain_name="${project_name}.${reponame}.${domain}"
+	
+	remove_virtual_host ${project_name}
+    remove_route53_record_set ${domain_name}
+	remove_database ${shop_db_name} ${core_db_name}
+	remove_root_dir ${project_name}
 }
 
 remove_virtual_host(){
@@ -52,6 +65,48 @@ remove_virtual_host(){
     apache_vhost_ssl_file="${apache_vhost_dir}$1.${reponame}-ssl.conf"
 
     echo "INFO: Deleting $apache_vhost_file"
+	rm $apache_vhost_file
+	
+	echo "INFO: Deleting $apache_vhost_ssl_file"
+	rm $apache_vhost_ssl_file
+	service apache2 reload
+}
+
+remove_route53_record_set(){
+    json_request='{
+          "Comment": "Create a batch of A record sets",
+          "Changes": [
+            {
+              "Action": "DELETE",
+              "ResourceRecordSet": {
+                "Name": '\"$1\"',
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [
+                  {
+                    "Value": '\"$dev_server_ip\"'
+                  }
+                ]
+              }
+            }
+          ]
+        }'
+    echo "INFO: Remove route53 record set: domain-name=$1"
+	aws route53 change-resource-record-sets  --hosted-zone-id $hosted_zone_id --change-batch "$json_request"
+}
+
+remove_database(){
+    echo "INFO: DROP $1 database..."
+    echo "DROP DATABASE IF EXISTS $1" | mysql -u root --password=${db_passwd}
+	
+	echo "INFO: DROP $2 database..."
+    echo "DROP DATABASE IF EXISTS $2" | mysql -u root --password=${db_passwd}
+}
+
+remove_root_dir(){
+    root_dir="${www_dir}$1.${reponame}"
+	echo "INFO: remove directory $root_dir"
+	rm -r $root_dir
 }
 
 get_branch_name(){
